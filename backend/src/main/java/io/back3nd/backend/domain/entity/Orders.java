72dd0a5ea701b6a47e216030a4606764e90e96b3
silkair <silkair1234@gmail.com>
2025-04-23 14:38:42 +0900

    feat : 주문 엔티티 및 배송 상태 설정

--- backend/src/main/java/io/back3nd/backend/domain/entity/Orders.java
+++ backend/src/main/java/io/back3nd/backend/domain/entity/Orders.java
@@ -1,4 +1,31 @@
 package io.back3nd.backend.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Orders {
+
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+
+    private String address;
+
+    private String zipcode;
+
+    private Status status;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt = LocalDateTime.now(); //아직 구현하지는 않을 예정
 }
---
+++ backend/src/main/java/io/back3nd/backend/domain/entity/Status.java
@@ -0,0 +1,7 @@
+package io.back3nd.backend.domain.entity;
+
+public enum Status {
+    RECEIVED,
+    SHIPPING,
+    COMPLETED //일단 넣어봄..ㅎㅎ
+}
