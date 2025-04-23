86f489ab7be0a185429aca7d68e64ceee288c913
silkair <silkair1234@gmail.com>
2025-04-23 21:57:16 +0900

    feat : 주문 생성 컨트롤러 구현

--- backend/src/main/java/io/back3nd/backend/domain/api/OrderController.java
+++ backend/src/main/java/io/back3nd/backend/domain/api/OrderController.java
@@ -1,4 +1,23 @@
 package io.back3nd.backend.domain.api;
 
+import io.back3nd.backend.domain.app.OrderService;
+import io.back3nd.backend.domain.dto.OrderRequest;
+import io.back3nd.backend.domain.dto.OrderResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/orders")
+    public ResponseEntity<OrderResponse> doOrder(@RequestBody OrderRequest orderRequest) {
+        OrderResponse orderResponse = orderService.doOrder(orderRequest);
+        return ResponseEntity.ok(orderResponse);
+    }
 }
