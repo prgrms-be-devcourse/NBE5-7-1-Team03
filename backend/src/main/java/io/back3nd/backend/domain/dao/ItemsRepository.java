eda3e492c43bc139665b57a7e5cc112ece48f8ed
silkair <silkair1234@gmail.com>
2025-04-23 21:40:15 +0900

    feat : 주문 생성 서비스 테스트 코드 작성

--- backend/src/main/java/io/back3nd/backend/domain/dao/ItemsRepository.java
+++ backend/src/main/java/io/back3nd/backend/domain/dao/ItemsRepository.java
@@ -3,5 +3,7 @@ package io.back3nd.backend.domain.dao;
 import io.back3nd.backend.domain.entity.Items;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemsRepository extends JpaRepository<Items, Long> {
+public interface ItemsRepository
+        //extends JpaRepository<Items, Long>
+{
 }
--- backend/src/test/java/io/back3nd/backend/domain/app/OrderServiceTests.java
+++ backend/src/test/java/io/back3nd/backend/domain/app/OrderServiceTests.java
@@ -1,7 +1,50 @@
 package io.back3nd.backend.domain.app;
 
+import io.back3nd.backend.domain.dao.OrdersRepository;
+import io.back3nd.backend.domain.dto.OrderRequest;
+import io.back3nd.backend.domain.dto.OrderResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
+@SpringBootTest
 class OrderServiceTests {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrdersRepository ordersRepository;
+
+    @Test
+    @DisplayName("주문하기")
+    void doOrdersTests() throws Exception {
+        OrderRequest request = new OrderRequest(
+                "example@example.com",
+                "인천광역시 연수구",
+                "12345",
+                List.of(1L,2L,3L)
+        );
+
+        OrderResponse orderResponse = orderService.doOrder(request);
+
+        assertThat(request).isNotNull();
+        assertThat(request.getEmail()).isEqualTo("example@example.com");
+        assertThat(request.getAddress()).isEqualTo("인천광역시 연수구");
+        assertThat(request.getZipcode()).isEqualTo("12345");
+
+        log.info("orderResponse.getId() = {}", orderResponse.getId());
+        log.info("orderResponse.getStatus() = {}", orderResponse.getStatus());
+
+    }
 
 }
