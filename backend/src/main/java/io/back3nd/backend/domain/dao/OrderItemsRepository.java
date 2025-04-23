d04655cde9d85674694eca0bce4ff93947bf40fb
2ternal <ddft97@gmail.com>
2025-04-23 16:51:10 +0900

    refactor: 사용하지 않는 dto 삭제 & 기존 코드 주석처리 제거

--- backend/src/main/java/io/back3nd/backend/domain/dao/OrderItemsRepository.java
+++ backend/src/main/java/io/back3nd/backend/domain/dao/OrderItemsRepository.java
@@ -3,7 +3,5 @@ package io.back3nd.backend.domain.dao;
 import io.back3nd.backend.domain.entity.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemsRepository
-//        extends JpaRepository<OrderItems, Long>
-{
+public interface OrderItemsRepository extends JpaRepository<OrderItems, Long> {
 }
--- backend/src/main/java/io/back3nd/backend/domain/dao/OrdersRepository.java
+++ backend/src/main/java/io/back3nd/backend/domain/dao/OrdersRepository.java
@@ -3,7 +3,5 @@ package io.back3nd.backend.domain.dao;
 import io.back3nd.backend.domain.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrdersRepository
-//        extends JpaRepository<Orders, Long>
-{
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
 }
