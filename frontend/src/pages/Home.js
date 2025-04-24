90942254fbde5fb6da00a89d6d4a688eb10a263d
zie-ning <jeeun03@gmail.com>
2025-04-24 10:11:40 +0900

    refact: 텍스트 수정

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,10 +6,10 @@ export default function Home(){
         <div className="container p-5 text-center">
           <div className="card style={{ height: '100vh' }}">
             <div className="card-header">
-              <h2 className="text-center mt-5 mb-3">Back3nd Coffee</h2>
+              <h2 className="text-center mt-5 mb-3">GC Coffee</h2>
             </div>
             <div className="card-body d-flex flex-column justify-content-center align-items-center p-4">
-              <p><Link to={`/admin`} className="btn btn-secondary mt-5 mb-3 px-4 py-2">관리자 페이지</Link></p>
+              <p><Link to={`/admin`} className="btn btn-secondary mt-5 mb-3 px-4 py-2">관리자 페이지g</Link></p>
               <p><Link to={`/order`} className="btn btn-dark px-4 py-2">커피 주문하기</Link></p>
             </div>
           </div>         
