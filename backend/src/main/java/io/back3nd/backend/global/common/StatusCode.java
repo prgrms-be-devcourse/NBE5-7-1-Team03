9000bf1341dd5f77ddb3755afacffbe98c28dcd0
silkair <silkair1234@gmail.com>
2025-04-25 10:38:32 +0900

    fix : 주문 조회 응답 값 수정(정보 추가 )

--- backend/src/main/java/io/back3nd/backend/global/common/StatusCode.java
+++ backend/src/main/java/io/back3nd/backend/global/common/StatusCode.java
@@ -14,7 +14,7 @@ public enum StatusCode {
     /* Orders */
     ORDER_SUCCESS(CREATED, "주문이 성공적으로 접수되었습니다."),
     ORDER_FOUND(OK,"주문 조회에 성공헀습니다."),
-    ORDER_DELETE(NO_CONTENT,"주문 취소에 성공했습니다."),
+    ORDER_DELETE(OK,"주문 취소에 성공했습니다."),
 
     /* Item */
     ITEM_CREATED(CREATED,"상품이 등록 되었습니다."),
