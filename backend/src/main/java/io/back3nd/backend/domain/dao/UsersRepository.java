fd2dcef4e847f17e772af940ba17a60473c9a8df
silkair <silkair1234@gmail.com>
2025-04-25 17:18:25 +0900

    feat : 유저 아이디 기반 주문 생성 테스트 코드 작성 및 테스트

--- backend/src/main/java/io/back3nd/backend/domain/dao/UsersRepository.java
+++ backend/src/main/java/io/back3nd/backend/domain/dao/UsersRepository.java
@@ -3,5 +3,8 @@ package io.back3nd.backend.domain.dao;
 import io.back3nd.backend.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
 }
--- backend/src/main/java/io/back3nd/backend/domain/dto/ItemRequest.java
+++ backend/src/main/java/io/back3nd/backend/domain/dto/ItemRequest.java
@@ -1,8 +1,10 @@
 package io.back3nd.backend.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ItemRequest {
 
     private String name;
